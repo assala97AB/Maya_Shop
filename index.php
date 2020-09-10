7c3495dcb3e5cb97b6e7fcc797e26c63e6183d46
assala97AB <69803479+assala97AB@users.noreply.github.com>
2020-09-10 22:08:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 
   <div class="mySlides fade">
     <div class="numbertext">1 / 3</div>
-    <img src="images/1.png" style="width:100%">
+    <img src="1.png" style="width:100%">
     <div class="text">Caption Text</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">2 / 3</div>
-    <img src="images/2.png" style="width:100%">
+    <img src="2.png" style="width:100%">
     <div class="text">Caption Two</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">3 / 3</div>
-    <img src="images/3.png" style="width:100%">
+    <img src="3.png" style="width:100%">
     <div class="text">Caption Three</div>
   </div>
 
